88b30440b4b40b82de9ab9bdcb1b91edb6df7251
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-02-22 00:01:15 +0000

    add

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const date = new Date();
 const hours = date.getHours();
 const minutes = date.getMinutes();
 const seconds = date.getSeconds();
+
+hoursElement.textContent = fixTime(hours);
+minutesElement.textContent = fixTime(minutes);
+secondsElement.textContent = fixTime(seconds);
+
+function fixTime(time){
+    return time < 10 ? '0' + time : time;
+}
